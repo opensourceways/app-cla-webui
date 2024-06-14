2a30aa6566f8ca0b06709c92765e83ba2bf69da7
邦邦邦邦 <15622356989@163.com>
2024-06-14 18:14:07 +0800

    fix: 解决打包错误

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 
 function resolve(dir) {
   return path.join(__dirname, dir);
@@ -7,51 +7,62 @@ function resolve(dir) {
 module.exports = {
   pwa: {
     iconPaths: {
-      favicon32: "favicon.ico",
-      favicon16: "favicon.ico",
-      appleTouchIcon: "favicon.ico",
-      maskIcon: "favicon.ico",
-      msTileImage: "favicon.ico",
+      favicon32: 'favicon.ico',
+      favicon16: 'favicon.ico',
+      appleTouchIcon: 'favicon.ico',
+      maskIcon: 'favicon.ico',
+      msTileImage: 'favicon.ico'
     },
     lintOnSave: false,
     productionSourceMap: false,
     devServer: {
-        hot: true,
-        https:true,
-        proxy: {
-            '/api': {
-                target:'https://clasign.test.myopeninfra.com',
-                secure: false,
-                changeOrigin: true,
-                // pathRewrite: {
-                //     '^/api': ''
-                // }
-            }
+      hot: true,
+      https: true,
+      proxy: {
+        '/api': {
+          target: 'https://clasign.test.myopeninfra.com',
+          secure: false,
+          changeOrigin: true
+          // pathRewrite: {
+          //     '^/api': ''
+          // }
         }
-    },
+      }
+    }
   },
   chainWebpack: (config) => {
-    const svgRule = config.module.rule("svg");
+    const svgRule = config.module.rule('svg');
     svgRule.uses.clear();
     svgRule
       .test(/\.svg$/)
-      .include.add(path.resolve(__dirname, "./src/icons"))
+      .include.add(path.resolve(__dirname, './src/icons'))
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]",
+        symbolId: 'icon-[name]'
       });
-    const fileRule = config.module.rule("file");
+    const fileRule = config.module.rule('file');
     fileRule.uses.clear();
     fileRule
       .test(/\.svg$/)
-      .exclude.add(path.resolve(__dirname, "./src/icons"))
+      .exclude.add(path.resolve(__dirname, './src/icons'))
       .end()
-      .use("file-loader")
-      .loader("file-loader");
+      .use('file-loader')
+      .loader('file-loader');
     config.resolve.alias
-      .set("@", resolve("src"))
-      .set("@components", resolve("src/components"));
-  },
+      .set('@', resolve('src'))
+      .set('@components', resolve('src/components'));
+
+    config.module
+      .rule('mjs')
+      .test(/\.mjs$/)
+      .include.add(/node_modules\/pdfjs-dist/) // 只处理指定的目录，以加快构建速度
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+      .options({
+        presets: ['@babel/preset-env']
+      });
+  }
 };
